7f02267e62dc0ed2fb6259acb48739e7f91a3a61
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-08-31 17:15:35 +0200

    Adds timestamps to entities and role find by name
    Adds create and update timestamps to Booking, Tenant, and User entities using JPA lifecycle events.
    This ensures that the creation and modification times are automatically managed.
    
    Also adds the possibility to find a Role by its name.

--- src/main/java/com/r16a/metis/booking/models/Booking.java
+++ src/main/java/com/r16a/metis/booking/models/Booking.java
@@ -45,4 +45,15 @@ public class Booking {
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/r16a/metis/identity/models/Tenant.java
+++ src/main/java/com/r16a/metis/identity/models/Tenant.java
@@ -19,4 +19,15 @@ public class Tenant {
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
