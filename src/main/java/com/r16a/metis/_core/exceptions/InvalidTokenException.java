dc09552ca474c5b4fbb6b506d5bfd172d474ecea
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-08-31 13:32:37 +0200

    Adds custom exception classes
    Introduces `InvalidTokenException` and `UserNotFoundException`
    as custom exception classes.
    
    These exceptions provide more specific error handling
    for authentication and user retrieval processes.

---
+++ src/main/java/com/r16a/metis/_core/exceptions/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.r16a.metis._core.exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
