68d79c189e3910a94ee4dc0a3c65c9872e07e805
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-08-31 16:48:41 +0200

    Updates exception constructors
    Modifies the constructors of `InvalidTokenException` and
    `UserNotFoundException` for clarity and consistency.
    
    `InvalidTokenException` now uses a default message.
    `UserNotFoundException` now takes user email as input
    for a more informative message.

--- src/main/java/com/r16a/metis/_core/exceptions/InvalidTokenException.java
+++ src/main/java/com/r16a/metis/_core/exceptions/InvalidTokenException.java
@@ -1,7 +1,7 @@
 package com.r16a.metis._core.exceptions;
 
 public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message) {
-        super(message);
+    public InvalidTokenException() {
+        super("Invalid token");
     }
 }
--- src/main/java/com/r16a/metis/_core/exceptions/UserNotFoundException.java
+++ src/main/java/com/r16a/metis/_core/exceptions/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.r16a.metis._core.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
+    public UserNotFoundException(String userEmail) {
+        super("User not found: " + userEmail);
     }
 }
