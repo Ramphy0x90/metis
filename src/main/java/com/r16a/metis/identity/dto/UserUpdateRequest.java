98d8711346bb25f12ee53eb5d011194182452e20
Ramphy Aquino Nova <ramphy_an@outlook.com>
2025-10-05 10:06:59 +0200

    min: update

--- src/main/java/com/r16a/metis/identity/dto/UserUpdateRequest.java
+++ src/main/java/com/r16a/metis/identity/dto/UserUpdateRequest.java
@@ -2,6 +2,8 @@ package com.r16a.metis.identity.dto;
 
 import com.r16a.metis.identity.models.UserRole;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,18 +18,23 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateRequest {
+    @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
     private String email;
 
+    @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
+    @NotBlank(message = "Name is required")
     private String name;
 
+    @NotBlank(message = "Surname is required")
     private String surname;
 
     private UUID tenantId;
 
+    @NotEmpty(message = "At least one role must be specified")
     private Set<UserRole> roles;
 }
 
